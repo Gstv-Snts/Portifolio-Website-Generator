e6d7c9f2cc6d5dc4f86a70474964271554c6ebef
Gustavo <gustavosantossilveira12360@gmail.com>
2023-10-01 18:25:41 -0300

    Initial config

---
+++ build/out/CMakeFiles/cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp.dir/home/gustavo/Documents/Portifolio-Website-Generator/src/main.cpp.o"
+  "CMakeFiles/cpp.dir/home/gustavo/Documents/Portifolio-Website-Generator/src/main.cpp.o.d"
+  "cpp"
+  "cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <stdlib.h>
+int main()
+{
+    printf("Portifolio website generator.");
+    return 0;
+}
